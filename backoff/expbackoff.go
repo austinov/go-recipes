730264bb161e25408406ead35480a26dcf4a0249
Andrey Ustinov <ustinov.post@gmail.com>
2016-10-30 17:55:51 +0300

    Work on refactoring

--- backoff/expbackoff.go
+++ backoff/expbackoff.go
@@ -2,9 +2,11 @@ package backoff
 
 import (
 	"math/rand"
+	"sync"
 	"time"
 )
 
+// Config defines the config for ExpBackoff
 type Config struct {
 	MinDelay time.Duration
 	MaxDelay time.Duration
@@ -12,13 +14,21 @@ type Config struct {
 	Jitter   float64
 }
 
+// ExpBackoff is a thread-safe implemention of
+// the backoff algorithm to exponential increase
+// the delay between repeated processes
+// in the case of unsuccessful attempts.
 type ExpBackoff struct {
-	config   Config
+	config Config
+
+	// this mutex protects delay and attempts
+	mu       sync.Mutex
 	delay    time.Duration
 	attempts uint64
 }
 
 var (
+	// DefaultConfig is the default ExpBackoff config.
 	DefaultConfig = Config{
 		100 * time.Millisecond,
 		10 * time.Minute,
@@ -28,10 +38,12 @@ var (
 	rnd = rand.New(rand.NewSource(time.Now().UnixNano()))
 )
 
+// NewExpBackoff returns an ExpBackoff with default configuration.
 func NewExpBackoff() *ExpBackoff {
 	return NewExpBackoffWithConfig(DefaultConfig)
 }
 
+// NewExpBackoffWithConfig returns an ExpBackoff by passed configuration.
 func NewExpBackoffWithConfig(config Config) *ExpBackoff {
 	return &ExpBackoff{
 		config: config,
@@ -39,17 +51,39 @@ func NewExpBackoffWithConfig(config Config) *ExpBackoff {
 	}
 }
 
-func (e *ExpBackoff) Reset() {
-	e.delay = e.config.MinDelay
-	e.attempts = 0
+// Delay waits for exponentially increased duration and send the current time
+// on the returned channel.
+func (e *ExpBackoff) Delay() <-chan time.Time {
+	e.mu.Lock()
+	currDelay := e.delay
+	nextDelay := currDelay
+	e.mu.Unlock()
+
+	nextDelay *= time.Duration(e.config.Expo)
+	if nextDelay > e.config.MaxDelay {
+		nextDelay = e.config.MaxDelay
+	}
+	normal := time.Duration(rnd.NormFloat64() * e.config.Jitter * float64(time.Millisecond))
+	nextDelay += normal
+
+	e.mu.Lock()
+	e.delay = nextDelay
+	e.attempts++
+	e.mu.Unlock()
+	return time.After(currDelay)
 }
 
-func (e *ExpBackoff) Delay() <-chan time.Time {
-	e.delay = e.delay * time.Duration(e.config.Expo)
-	if e.delay > e.config.MaxDelay {
-		e.delay = e.config.MaxDelay
-	}
-	normal := time.Duration(rnd.Float64() * e.config.Jitter * float64(time.Second))
-	e.delay = e.delay + normal
-	return time.After(e.delay)
+// Reset sets current delay the minimum value
+func (e *ExpBackoff) Reset() {
+	e.mu.Lock()
+	e.attempts = 0
+	e.delay = e.config.MinDelay
+	e.mu.Unlock()
+}
+
+// Attempts returns number of unsuccessful attempts
+func (e *ExpBackoff) Attempts() uint64 {
+	e.mu.Lock()
+	defer e.mu.Unlock()
+	return e.attempts
 }
--- backoff/expbackoff_test.go
+++ backoff/expbackoff_test.go
@@ -2,26 +2,82 @@ package backoff
 
 import (
 	"fmt"
+	"math"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestDelay(t *testing.T) {
+	assert := assert.New(t)
 	eb := NewExpBackoffWithConfig(Config{
-		100 * time.Millisecond,
-		5 * time.Second,
-		2.0,
-		0.1,
+		MinDelay: 100 * time.Millisecond,
+		MaxDelay: 5 * time.Second,
+		Expo:     2.0,
+		Jitter:   0.1,
 	})
-	fmt.Println(<-eb.Delay())
-	fmt.Println(<-eb.Delay())
-	fmt.Println(<-eb.Delay())
-	fmt.Println(<-eb.Delay())
-	fmt.Println(<-eb.Delay())
-	fmt.Println(<-eb.Delay())
-	fmt.Println(<-eb.Delay())
-	fmt.Println(<-eb.Delay())
-	eb.Reset()
-	fmt.Println(<-eb.Delay())
-	fmt.Println(<-eb.Delay())
+	assert.NotNil(eb)
+
+	cases := []struct {
+		name     string
+		expected time.Duration
+		reset    bool
+	}{
+		{
+			name:     "1st attempt",
+			expected: 100 * time.Millisecond,
+		},
+		{
+			name:     "2nd attempt",
+			expected: 200 * time.Millisecond,
+		},
+		{
+			name:     "3rd attempt",
+			expected: 400 * time.Millisecond,
+		},
+		{
+			name:     "4th attempt",
+			expected: 800 * time.Millisecond,
+		},
+		{
+			name:     "5th attempt",
+			expected: 1600 * time.Millisecond,
+		},
+		{
+			name:     "6th attempt",
+			expected: 3200 * time.Millisecond,
+		},
+		{
+			name:     "7th attempt",
+			expected: 5000 * time.Millisecond,
+		},
+		{
+			name:     "8th attempt",
+			expected: 5000 * time.Millisecond,
+		},
+		{
+			name:     "9th attempt",
+			expected: 100 * time.Millisecond,
+			reset:    true,
+		},
+		{
+			name:     "10th attempt",
+			expected: 200 * time.Millisecond,
+		},
+	}
+	for i, c := range cases {
+		if c.reset {
+			eb.Reset()
+		}
+		t1 := time.Now()
+		t2 := <-eb.Delay()
+		delta := math.Ceil(float64(c.expected) * 0.01)
+		t.Logf("T%d %v, %v, %v", i+1, c.expected, t2.Sub(t1), c.expected-t2.Sub(t1))
+		assert.InDelta(
+			float64(c.expected),
+			float64(t2.Sub(t1)),
+			delta,
+			fmt.Sprintf("delay %d attempt ~%v", i+1, c.expected))
+	}
 }
